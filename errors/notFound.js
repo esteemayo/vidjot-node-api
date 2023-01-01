f09aee9413d85c35cde792c11b1d4850455e3cb4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-01 03:41:51 +0100

    fix: change from common.js to es6 module

--- errors/notFound.js
+++ errors/notFound.js
@@ -1,5 +1,6 @@
-const { StatusCodes } = require('http-status-codes');
-const AppError = require('./appError');
+/* eslint-disable */
+import { StatusCodes } from 'http-status-codes';
+import AppError from './appError.js';
 
 class NotFoundError extends AppError {
   constructor(message) {
@@ -10,4 +11,4 @@ class NotFoundError extends AppError {
   }
 }
 
-module.exports = NotFoundError;
+export default NotFoundError;
