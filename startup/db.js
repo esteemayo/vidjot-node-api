19d07c4aa312dfe1f7fd7a60802f91c878df1780
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-02 17:45:40 +0100

    fix: configure dotenv

--- startup/db.js
+++ startup/db.js
@@ -1,5 +1,8 @@
 /* eslint-disable */
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+
+dotenv.config({ path: './config.env' });
 
 const devEnv = process.env !== 'production';
 const { DATABASE, DATABASE_LOCAL, DATABASE_PASSWORD } = process.env;
